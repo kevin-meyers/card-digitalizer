591ca574c3d37a4f3c2a00df6bcfdd2cdc6f7fee
Kevin <kevinm1776@gmail.com>
2020-03-16 19:33:35 -0400

    Added ericas repo as a submodule.

--- card_organizer/card/models.py
+++ card_organizer/card/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Card(models.Model):
     name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    type_1 = models.CharField(max_length=20)
+    type_2 = models.CharField(max_length=20, null=True, blank=True)
+
     price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
---
+++ kevs_frontend/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import axios from "axios";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading"
+    };
+  }
+
+  componentDidMount() {
+    axios.get("/api/card")
+      .then(response => {
+        if (response.status > 400) {
+          return this.setState(() => {
+            return { placeholder: "Something went wrong!" };
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.data.map(card => {
+          return (
+            <li key={card.id}>
+              {card.name} - {card.price}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default App;
+
+const container = document.getElementById("app");
+render(<App />, container);
