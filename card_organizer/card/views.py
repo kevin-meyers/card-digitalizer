91c9d52a7395e0a8493280a804680019f5ad6896
Kevin <kevinm1776@gmail.com>
2020-05-09 17:33:13 -0400

    Updates.

--- card_organizer/card/views.py
+++ card_organizer/card/views.py
@@ -9,33 +9,6 @@ from rest_framework_simplejwt.backends import TokenBackend
 
 tb = TokenBackend('HS256')
 
-
-class myAuth(authentication.TokenAuthentication):
-    def authenticate(self, request):
-        token = request.COOKIES.get('jwt_token')
-        data = tb.decode(token, verify=None)
-        print(data)
-        auth = ['Token', data['jti']]
-
-        if not auth or auth[0].lower() != self.keyword.lower().encode():
-            return None
-
-        if len(auth) == 1:
-            msg = _('Invalid token header. No credentials provided.')
-            raise exceptions.AuthenticationFailed(msg)
-        elif len(auth) > 2:
-            msg = _('Invalid token header. Token string should not contain spaces.')
-            raise exceptions.AuthenticationFailed(msg)
-
-        try:
-            token = auth[1].decode()
-        except UnicodeError:
-            msg = _('Invalid token header. Token string should not contain invalid characters.')
-            raise exceptions.AuthenticationFailed(msg)
-
-        return self.authenticate_credentials(token)
-
-
 class PokemonList(generics.ListAPIView):
     serializer_class = PokemonSerializer
     authentication_classes = []  # [myAuth]
@@ -43,6 +16,8 @@ class PokemonList(generics.ListAPIView):
 
     def get_queryset(self):
         username = self.request.COOKIES.get('username')
+        print(username)
+        print(self.request.COOKIES)
         user = User.objects.get(username=username)
 
         return Pokemon.objects.filter(user=user)
